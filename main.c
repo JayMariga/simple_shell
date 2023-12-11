7c57d3da462fc6095aff30d1e8590f5e0c94c0ba
joseph <marigajoseph8046@gmail.com>
2023-12-10 22:48:20 -0800

    Entry point for a simple shell

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for a simple shell
+ * @ac: This is the no. of command-line arguments
+ * @av: Array of strings rep command-line arguments
+ * @env: Array of strings rep the environment.
+ *
+ * Return: 0 on success
+ */
+
+int main(int ac, char **av, char **env)
+{
+        /* Suppress compiler warning on unused parameters. */
+        (void)ac;
+        (void)av;
+
+        /* Call function to start the prompt. */
+        prompt(env);
+
+        return (0);
+}
