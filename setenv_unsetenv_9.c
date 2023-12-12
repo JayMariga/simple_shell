e3920ae8675ed11cd378dcdcd1a1f3757e47e8ae
joseph <marigajoseph8046@gmail.com>
2023-12-12 03:42:57 -0800

    creating or modifying existing environment variablr

---
+++ setenv_unsetenv_9.c
@@ -0,0 +1,135 @@
+#include "shell.h"
+
+/**
+ * find_env - Searches for a specific envitonmrntal variable
+ * in a linked list.
+ * @env: Pointer to the environmental variable linked list
+ * @str: variable name that is to be searched.
+ *
+ * Return: idx of node in linked list
+ */
+
+int find_env(list_t *env, char *str)
+{
+	int j = 0, index = 0;
+
+	while (env != NULL)
+	{
+		j = 0;
+		/* finding the desired env var */
+		while ((env->var)[j] == str[j])
+			j++;
+		/* And if it matches , break, and return idx */
+		if (str[j] == '\0')
+			break;
+		env = env->next;
+		index++;
+	}
+	if (env == NULL)
+		return (-1);
+
+	return (index);
+}
+
+/**
+ * _setenv - Modifies an environmental variable in linked list
+ * @env: Pointer to linked list
+ * @str: user's input command (e.g. "setenv USER Superman")
+ *
+ * Return: 0 If it's a success, 1 on failure
+ */
+
+int _setenv(list_t **env, char **str)
+{
+	int index = 0, j = 0;
+	char *cat;
+	list_t *holder;
+
+	/* Checking if arguments are enough */
+	if (str[1] == NULL || str[2] == NULL)
+	{
+		write(STDOUT_FILENO, "Too few arguments\n", 18);
+		free_double_ptr(str);
+		return (-1);
+	}
+
+	/* Concatenates string to form new node data */
+	new_node_data = _strdup(str[1]);
+	new_node_data = _strcat(cat, "=");
+	new_node_data = _strcat(cat, str[2]);
+
+	/* Find the index to traverse to the new node */
+	index = find_env(*env, str[1]);
+
+	/* Checking if environment var exists */
+	if (index == -1)
+	{
+		add_end_node(env, new_node_data);
+	}
+	else
+	{
+		holder = *env;
+
+		/* Traverse node in the linked list */
+		while (j < index)
+		{
+			holder = holder->next;
+			j++;
+		}
+
+		/* Free previous allocated data and assign new data */
+		free(holder->var);
+		holder->var = _strdup(cat);
+	}
+
+	/* free memory and returning success */
+	free(new_node_data);
+	free_double_ptr(str);
+	return (0);
+}
+
+
+
+/**
+ * _unsetenv - Should remove node in env linked list
+ * @env: Pointer to a linked list
+ * @str: User's input command (e.g. "unsetenv MAIL")
+ *
+ * Return: 0 on success
+ */
+
+int _unsetenv(list_t **env, char **str)
+{
+	int index = 0, j = 0;
+
+	/* Are the arguments enough */
+	if (str[1] == NULL)
+	{
+		write(STDOUT_FILENO, "Too few arguments\n", 18);
+		free_double_ptr(str);
+		return (-1);
+	}
+
+	/* Get index of the node to delete */
+	index = find_env(*env, str[1]);
+
+	/* Free memory and check if index is not found, hence error */
+	free_double_ptr(str);
+	if (index == -1)
+	{
+		write(STDOUT_FILENO, "Cannot find\n", 12);
+		return (-1);
+	}
+
+	/* delete the node a a specified index */
+	j = delete_nodeint_at_index(env, index);
+
+	/* Error if deletion failed */
+	if (j == -1)
+	{
+		write(STDOUT_FILENO, "Cannot find\n", 12);
+		return (-1);
+	}
+
+	return (0);
+}
--- shell.h
+++ shell.h
@@ -24,6 +24,9 @@ char *get_env(char *str, list_t *env);
 char *c_strdup(char *str, int cs);
 int __exit(char **str, list_t *env, int num, char **command);
 int c_atoi(char *s);
+int _unsetenv(list_t **env, char **str);
+int find_env(list_t *env, char *str);
+int _setenv(list_t **env, char **str);
 
 
 
