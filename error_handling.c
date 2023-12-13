765b2c8cf0f07c480b63783c210a337b34ed9675
joseph <marigajoseph8046@gmail.com>
2023-12-13 06:54:15 -0800

    working on errors and printing errors

---
+++ error_handling.c
@@ -0,0 +1,104 @@
+#include "shell.h"
+
+/**
+ * not_found - write error when found ("sh: 1: lss: not found")
+ * @str: The user's typed command
+ * @c_n: The nth user's typed command
+ * @env: The env variables linked list to write shell name
+ */
+
+void not_found(char *str, int c_n, list_t *env)
+{
+	int count = 0;
+	char *shell, *num;
+
+	/* The shell name to write */
+	shell = get_env("_", env);
+	while (shell[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, shell, count);
+	free(shell);
+	write(STDOUT_FILENO, ": ", 2);
+
+	/* We convert command line number to str to write */
+	num = int_to_string(c_n);
+	count = 0;
+	while (num[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, num, count);
+	free(num);
+	write(STDOUT_FILENO, ": ", 2);
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, "not found\n", 10);
+}
+
+/**
+ * cant_cd_to - write error when found ("sh: 2: cd: can't cd to xyz")
+ * @str: The user's typed argt after the command cd
+ * @c_n: The nth user's typed cmd.
+ * @env: The env variables linked list to write shell name
+ */
+
+void cant_cd_to(char *str, int c_n, list_t *env)
+{
+	int count = 0;
+	char *shell, *num;
+
+	shell = get_env("_", env);
+	while (shell[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, shell, count);
+	free(shell);
+	write(STDOUT_FILENO, ": ", 2);
+	num = int_to_string(c_n);
+	count = 0;
+	while (num[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, num, count);
+	free(num);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, "cd: can't cd to ", 16);
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, "\n", 1);
+}
+
+/**
+ * illegal_number - write error when found
+ * ("sh: 3: exit: Illegal number abc (or -1)")
+ * @str: The user's typed argt after the cmd exit
+ * @c_n: The nth user's typed cmd
+ * @env: The env variables linked list to write shell name
+ */
+
+void illegal_number(char *str, int c_n, list_t *env)
+{
+	int count = 0;
+	char *shell = NULL, *num = NULL;
+
+	shell = get_env("_", env);
+	while (shell[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, shell, count);
+	free(shell);
+	write(STDOUT_FILENO, ": ", 2);
+	num = int_to_string(c_n);
+	count = 0;
+	while (num[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, num, count);
+	free(num);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, "exit: Illegal number: ", 22);
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, "\n", 1);
+}
